efb2650a7b69c7041ba64d05eabef550d74288bc
robertelving <vivalaklask@gmail.com>
2016-12-19 11:12:32 +0100

    Added bank

---
+++ SOAPBank/src/main/java/com/cphbusiness/soapbank/BankLoanService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cphbusiness.soapbank;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Skroget
+ */
+@WebService(serviceName = "BankLoanService")
+public class BankLoanService {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getInterestRate")
+    public int getInterestRate(@WebParam(name = "ssn") String ssn, @WebParam(name = "creditscore") int creditscore, @WebParam(name = "amount") double amount, @WebParam(name = "duration") int duration) {
+        return (int)(Math.random()*20);
+    }
+}
